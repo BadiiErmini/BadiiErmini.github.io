8cd0ab7c2c51d4ebb4a1d08c03b97b9090a5cae5
filippo.ermini <filippo.ermini@gmail.com>
2017-01-24 00:19:03 +0100

    aggiustamento

--- js/dataManager.js
+++ js/dataManager.js
@@ -4,7 +4,6 @@ app.controller('layoutController', function($scope, $http) {
     $http.get("services/dm_getResources.php?res=layout_contents")
     .then(function (response) {$scope.layouts = response.data;});
     $scope.toggle = function($id) {
-    	alert($id);
     	$('#'+$id).collapse('toggle');
     };
 });
